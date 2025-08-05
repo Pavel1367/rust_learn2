d1b8aa23df64fb05981923c04c339dd248173f22
pavel1367 <pashka1367@bk.ru>
2025-08-05 21:00:43 +0300

    fibonacci via cache

--- fibonaci/src/main.rs
+++ fibonaci/src/main.rs
@@ -1,3 +1,17 @@
+use std::collections::HashMap;
 fn main() {
-    println!("Hello, world!");
+    let mut cache: HashMap<u32, u32> = HashMap::new();
+    println!("{}", fibonacci_cache(45, &mut cache))
+}
+
+fn fibonacci_cache(n: u32, cache: &mut HashMap<u32, u32>) -> u32 {
+    if n == 0 || n == 1 {
+       return  n
+    };
+    if let Some(value) = cache.get(&n) {
+       return  *value
+    };
+    let result = fibonacci_cache(n-1, cache) + fibonacci_cache(n-2, cache);
+    cache.insert(n, result);
+    result
 }
